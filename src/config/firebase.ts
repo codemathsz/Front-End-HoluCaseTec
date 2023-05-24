72e6aaf7c9937969d9c319d95a00c2c146f11c77
codemathsz <mo284891@gmail.com>
2023-05-24 16:55:48 -0300

    .

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpodXL3O9vRz9njOPkvItEjJXZH8IzJV8',
+  authDomain: 'apiholu-e0e86.firebaseapp.com',
+  databaseURL: 'https://apiholu-e0e86-default-rtdb.firebaseio.com',
+  projectId: 'apiholu-e0e86',
+  storageBucket: 'apiholu-e0e86.appspot.com',
+  messagingSenderId: '1044846455907',
+  appId: '1:1044846455907:web:d2f896fd8c5158741f1c56',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
